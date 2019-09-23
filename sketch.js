519adff25f88f081035896be956a26d6e9422a05
helizi <Helizi@HELIA.local>
2019-09-23 15:12:24 +0200

    code

--- sketch.js
+++ sketch.js
@@ -1,11 +1,24 @@
 function preload(){
-  // put preload code here
+
+
 }
 
 function setup() {
-  // put setup code here
+  createCanvas(windowWidth, windowHeight);
+  background("#2F4F4F");
+
+  angleMode(DEGREES);
+  frameRate(12);
 }
 
 function draw() {
-  // put drawing code here
+
+  translate(width/2, height/2);
+  rotate(90);
+  strokeWeight(2);
+  stroke(lerpColor(color("#EEE8AA"), color("#00CED1"), frameCount/60));
+  line(width/6, 0, cos(frameCount*6) * width/6, sin(frameCount*6) * width/6);
+  if (frameCount == 60) {
+    noLoop();
+}
 }
